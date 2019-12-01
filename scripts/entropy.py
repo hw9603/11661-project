cdca04f6645789198b964337f76fd2d4d25a7b33
Wen He <wenhe@andrew.cmu.edu>
2019-12-01 13:20:49 -0500

    write stats to file

--- scripts/entropy.py
+++ scripts/entropy.py
@@ -2,14 +2,26 @@ import utils
 import math
 import sys
 
+def write_stats_to_file(stats, outfile="results/stats.csv"):
+    with open(outfile, "w") as f:
+        f.write("word\tfreq\tprob\tentropy\n")
+        for word in sorted(stats.items(), key=lambda kv: kv[1][0], reverse=True):
+            freq, prob, entropy = word[1]
+            word = word[0]
+            f.write(word + "\t" + str(freq) + "\t" +
+                    str(prob) + "\t" + str(entropy) + "\n")
+
 def calc_word_entropy(filename):
     data = utils.read_data(filename)
     wordfreq, total_counts = utils.data2wordfreq(data)
     entropy = 0
+    stats = {}
     for word in wordfreq:
         freq = wordfreq[word]
         prob = freq / total_counts
+        stats[word] = [freq, prob, -math.log(prob, 2)]
         entropy -= prob * math.log(prob, 2)
+    write_stats_to_file(stats)
     return entropy
 
 
