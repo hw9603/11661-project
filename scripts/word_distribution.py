cabc58a6662bc7c2f9cbc06f9c4668714cc1c03f
linyiii <chenlinyi1996@yahoo.com>
2019-12-04 13:29:17 -0500

    add heaps law

--- scripts/word_distribution.py
+++ scripts/word_distribution.py
@@ -1,4 +1,5 @@
 import csv
+import pandas
 
 # import csv
 # with open('some.csv', 'w', newline='') as f:
@@ -20,7 +21,7 @@ def get_word_freq(data_file):
             dic[word] = 1
     ori_file.close()
     # print(type(dic))
-    return dic, tot
+    return dic, tot, len(dic.keys())
 
 
 # returns {freq:word}
@@ -36,15 +37,28 @@ def get_freq_word(word_freq_dic):
 
 
 def main(data_file_names, stats_file_name):
+    k = 30
+    beta = 0.5
     with open(stats_file_name, "w+") as f:
         writer = csv.writer(f)
-        writer.writerow(['file_name', 'total_words','unique_word_percentage'])
+        writer.writerow(['file_name', 'total_words','unique_word_percentage',
+                         'actual_distinct', 'predicted_distinct', 'differnce'])
         for data_file_name in data_file_names:
-            word_freq, tot = get_word_freq(data_file_name)
+            word_freq, tot, tot_distinct = get_word_freq(data_file_name)
             # in case no word occurs only once
+
+            # calculate unique word percentage
             uniq_words = get_freq_word(word_freq).get(1, [])
             uniq_prec = (len(uniq_words) * 1.0) / tot
-            writer.writerow([data_file_name.split('/')[-1], str(int(tot)), str(uniq_prec)])
+
+            # calculate Heaps' Law predicted distinct count
+            pred = k * (tot ** beta)
+            # calculate prediction difference
+            diff = ((pred - tot_distinct) * 1.0) / tot_distinct
+
+            writer.writerow([data_file_name.split('/')[-1], str(int(tot)), str(uniq_prec),
+                             str(tot_distinct), str(pred), str(diff)])
+
 
 
 if __name__ == "__main__":
